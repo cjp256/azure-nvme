021933146f6b567a8dc14c7de848d4d74c29ad24
Chris Patterson <cpatterson@microsoft.com>
2023-12-27 11:44:38 -0500

    WIP

--- reference/c/azure-nvme-print-vs.c
+++ reference/c/azure-nvme-print-vs.c
@@ -1,3 +1,4 @@
+#include <ctype.h>
 #include <dirent.h>
 #include <fcntl.h>
 #include <limits.h>
@@ -111,7 +112,7 @@ void debug_environment_variables() {
  * Anything beyond the terminating null-byte is currently undefined and
  * consequently ignored.
  */
-const char *query_namespace_vs(const char *namespace_path) {
+char *query_namespace_vs(const char *namespace_path) {
     struct nvme_id_ns *ns = NULL;
     unsigned int nsid = get_namespace_id_for_path(namespace_path);
 
@@ -143,7 +144,7 @@ const char *query_namespace_vs(const char *namespace_path) {
     }
     close(fd);
 
-    const char *vs = strndup((const char *)ns->vs, sizeof(ns->vs));
+    char *vs = strndup((const char *)ns->vs, sizeof(ns->vs));
     free(ns);
 
     return vs;
@@ -285,6 +286,81 @@ int enumerate_azure_nvme_controllers() {
     return 0;
 }
 
+/**
+ * Print a udev key-value pair for environment variable.
+ */
+void print_udev_key_value(const char *key, const char *value) {
+    printf("AZURE_NVME_");
+    while (*key) {
+        putchar(toupper((unsigned char)*key));
+        key++;
+    }
+    printf("=%s\n", value);
+}
+
+/**
+ * Print udev environment variables for all vs properties.
+ * 
+ * Example parsing for vs:
+ *     type=local,index=2,name=nvme-600G-2
+ * 
+ * Environment variables printed include:
+ *     AZURE_NVME_TYPE=local
+ *     AZURE_NVME_INDEX=2
+ *     AZURE_NVME_NAME=nvme-600G-2
+ */
+int parse_vs_for_udev_import(char *vs) {
+    char *vs_copy = strdup(vs);
+    char *outer_saveptr = NULL;
+    char *inner_saveptr = NULL;
+
+    char *pair = strtok_r(vs_copy, ",", &outer_saveptr);
+    while (pair != NULL) {
+        char *key = strtok_r(pair, "=", &inner_saveptr);
+        char *value = strtok_r(NULL, "=", &inner_saveptr);
+
+        if (key == NULL || value == NULL) {
+            fprintf(stderr, "failed to parse key-value pair: %s\n", pair);
+            free(vs_copy);
+            return 1;
+        }
+
+        print_udev_key_value(key, value);
+        pair = strtok_r(NULL, ",", &outer_saveptr);
+    }
+
+    free(vs_copy);
+    return 0;
+}
+
+/**
+ * Execute udev mode, printing out environment variables for import.
+ * 
+ * AZURE_NVME_VS: <vendor specific data>
+ * AZURE_NVME_TYPE: <type, if specified>
+ * AZURE_NVME_INDEX: <index for data disk, if specified>
+ * AZURE_NVME_NAME: <name for disk, if specified>
+ * etc...
+ */
+int execute_udev_import(void) {
+    const char* dev_name = getenv("DEVNAME");
+    if (dev_name == NULL) {
+        fprintf(stderr, "environment variable 'DEVNAME' not set\n");
+        return 1;
+    }
+
+    char *vs = query_namespace_vs(dev_name);
+    if (vs == NULL) {
+        fprintf(stderr, "failed to query namespace vendor-specific data\n");
+        return 1;
+    }
+
+    printf("AZURE_NVME_VS=%s\n", vs);
+    parse_vs_for_udev_import(vs);
+    free(vs);
+    return 0;
+}
+
 int main(int argc, const char **argv) {
     for (int i = 1; i < argc; i++) {
         if (strcmp(argv[i], "--debug") == 0) {
@@ -293,7 +369,7 @@ int main(int argc, const char **argv) {
         }
         if (strcmp(argv[i], "--udev") == 0) {
             udev_mode = true;
-            break;
+            continue;
         }
     }
 
@@ -301,5 +377,9 @@ int main(int argc, const char **argv) {
         debug_environment_variables();
     }
 
-    return enumerate_azure_nvme_controllers();
+    if (udev_mode) {
+        return execute_udev_import();
+    } else {
+        return enumerate_azure_nvme_controllers();
+    }
 }
